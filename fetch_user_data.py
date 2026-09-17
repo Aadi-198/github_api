80bc02b37bf4c25314532ba7069c1f876456b62d
Aadi-Gupta928 <aadishresth@gmail.com>
2026-09-17 19:49:11 +0530

    fetch user's details

--- fetch_user_data.py
+++ fetch_user_data.py
@@ -9,6 +9,12 @@ user_url = home_url + user_name
 response = requests.get(user_url)
 
 if response.status_code == 200:
-    print(f"Fetching {user_name}'s data")
+    user_data = response.json()
+    print(f"Fetching {user_name}'s data ...\n")
+    print(f"User name : {user_data.get('name')}")
+    print(f"Name : {user_data.get('login')}")
+    print(f"About : {user_data.get('bio')}")
+    print(f"Location : {user_data.get('location')}")
+    print(f"Number of public repos : {user_data.get('public_repos')}\n")
 else:
     print(f"Failed to fetch {user_name}'s data.\nError Code {response.status_code} !")

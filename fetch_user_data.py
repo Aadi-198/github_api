b4be0a91d8d4c3d73bdecd3dce194f6afe77c6b6
Pro <aadishresth@gmail.com>
2026-09-17 16:10:16 +0530

    Make request to github's server via github API

---
+++ fetch_user_data.py
@@ -0,0 +1,14 @@
+import requests
+
+home_url = "http://api.github.com/users/"
+
+user_name = input("Enter your GitHub Username:\t")
+
+user_url = home_url + user_name
+
+response = requests.get(user_url)
+
+if response.status_code == 200:
+    print(f"Fetching {user_name}'s data")
+else:
+    print(f"Failed to fetch {user_name}'s data.\nError Code {response.status_code} !")
